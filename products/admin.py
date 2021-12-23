cd200e0367b129c1db30882dfb2699e2cbe18080
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-12-23 06:43:37 +0000

    Customise admin by displaying list of product fields and adding meta class for fixing plural spelling

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'brand',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
