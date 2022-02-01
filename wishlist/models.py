e0571a20658c109b2aea35b77ffa75b07e2e8e22
cosminaserbanica <cosmina.serbanica@yahoo.com>
2022-02-01 09:09:57 +0000

    Create user wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class WishList(models.Model):
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.product_sku
