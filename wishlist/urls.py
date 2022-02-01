e0639de3dddab8c1d54b0d046aaa119dc173efec
cosminaserbanica <cosmina.serbanica@yahoo.com>
2022-02-01 09:11:17 +0000

    Add url paths for wishlist functionality

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from bag import views as bag_views
 
 urlpatterns = [
-    path('', views.view_wishlist, name='view_wishlist')
+    path("", views.wishlist, name="wishlist"),
+    path('add_bag/<item_id>/', bag_views.add_to_bag, name='add_bag'),
+    path('remove/<wishlist_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist')
 ]
